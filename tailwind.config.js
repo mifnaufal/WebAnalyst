b9c00c408f428000943ab271616f728ad7649ebb
alxyz12 <naufalhhdd@gmail.com>
2025-11-03 09:20:52 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}", // Memastikan semua file di src terbaca
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
